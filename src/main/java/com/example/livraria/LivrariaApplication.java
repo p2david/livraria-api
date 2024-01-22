f63cdb9c26f3bab2d9b718109f2fc21e87be12e8
p2david <patricio2david@gmail.com>
2024-01-23 00:34:42 +0200

    commit inicial

---
+++ src/main/java/com/example/livraria/LivrariaApplication.java
@@ -0,0 +1,19 @@
+package com.example.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaApplication {
+
+	
+	public static void main(String[] args) {
+		//SpringApplication.run(LivrariaApplication.class, args);
+		 int a=2;
+			int b=2;
+			int c;
+		c=a+b;
+		System.out.println("ovalor do c="+c);
+	}
+
+}
